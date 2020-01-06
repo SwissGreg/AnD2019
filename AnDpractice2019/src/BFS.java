be703a4ea527f6a8e86cc12029f1a894c7330278
Greg <rgregory@student.ethz.ch>
2020-01-06 10:09:35 +0100

    BFS Implementation

--- AnDpractice2019/src/BFS.java
+++ AnDpractice2019/src/BFS.java
@@ -1,3 +1,4 @@
+
 /*==============================================================================================================================
  * 
  * Author: Gregory Rozanski
@@ -11,59 +12,62 @@
  ===============================================================================================================================*/
 import java.util.ArrayList;
 import java.util.ListIterator;
+
 public class BFS {
 	static Graph g;
+
 	public static void main(String[] args) {
-		g  = new Graph(6);
+		g = new Graph(6);
 		g.addEdge(0, 1);
 		g.addEdge(1, 2);
 		g.addEdge(2, 3);
 		g.addEdge(4, 5);
-		System.out.println(bfs(2,4));
+		System.out.println(bfs(2, 4));
 	}
-	public static boolean bfs(int u,int t) {
-		boolean[] visited = new boolean[g.adj.length];	
+
+	public static boolean bfs(int u, int t) {
+		boolean[] visited = new boolean[g.adj.length];
 		ArrayList<Integer> queue = new ArrayList();
 		queue.add(u);
-		if(u == t) {
+		if (u == t) {
 			return true;
 		}
-		while(!queue.isEmpty()) {
+		while (!queue.isEmpty()) {
 			int curr = queue.get(0);
 			queue.remove(0);
 			visited[curr] = true;
 			ListIterator<Integer> miterator = g.adj[curr].listIterator();
-			while(miterator.hasNext()) {
+			while (miterator.hasNext()) {
 				int next = miterator.next();
-				if(next == t) {
+				if (next == t) {
 					return true;
 				}
-				if(!visited[next]) {
-				queue.add(next);
+				if (!visited[next]) {
+					queue.add(next);
 				}
 			}
 		}
 		return false;
+
+	}
+	static class Graph {
+		int V;
+		ArrayList<Integer>[] adj;
+		
+		public Graph(int v) {
+			V = v;
+			adj = new ArrayList[v];
+			for (int i = 0; i < adj.length; i++) {
+				adj[i] = new ArrayList<Integer>();
+				
+			}
+		}
+		
+		public void addEdge(int u, int v) {
+			this.adj[u].add(v);
+			this.adj[v].add(u);
+		}
 		
 	}
 }
 
-class Graph{
-	int V;
-	ArrayList<Integer>[] adj;
-	
-	public Graph(int v){
-		V = v;
-		adj = new ArrayList[v];
-		for(int i = 0; i<adj.length;i++) {
-			adj[i] = new ArrayList<Integer>();
-			
-		}
-	}
-	
-	public void addEdge(int u, int v) {
-		this.adj[u].add(v);
-		this.adj[v].add(u);
-	}
-	
-}
