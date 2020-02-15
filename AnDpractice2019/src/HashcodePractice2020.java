96119cb8605926e24dca61e4558317ce896a95c5
Greg <rgregory@student.ethz.ch>
2020-02-15 17:30:07 +0100

    HashcodePractice2020 input/output template

--- AnDpractice2019/src/HashcodePractice2020.java
+++ AnDpractice2019/src/HashcodePractice2020.java
@@ -6,15 +6,41 @@ import java.util.Scanner;
 public class HashcodePractice2020 {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		File file = new File("input.txt");
-		Scanner scan = new Scanner(file);
-		PrintStream out = new PrintStream("output.txt");
+		File fileA = new File("inputA.txt");
+		File fileB = new File("inputB.txt");
+		File fileC = new File("inputC.txt");
+		File fileD = new File("inputD.txt");
+		File fileE = new File("inputE.txt");
+		Scanner scan = new Scanner(fileA);
+		PrintStream out = new PrintStream("outputA.txt");
+		readSolve(scan,out);
+		scan = new Scanner(fileB);
+		out = new PrintStream("outputB.txt");
+		readSolve(scan,out);
+		scan = new Scanner(fileC);
+		out = new PrintStream("outputC.txt");
+		readSolve(scan,out);
+		scan = new Scanner(fileD);
+		out = new PrintStream("outputD.txt");
+		readSolve(scan,out);
+		scan = new Scanner(fileE);
+		out = new PrintStream("outputE.txt");
 		readSolve(scan,out);
 
 	}
 	
 	public static void readSolve(Scanner in,PrintStream out) {
+		int N = in.nextInt();
+		int K = in.nextInt();
+		int[] slices = new int[N];
+		for(int i = 0; i< N;i++) {
+			slices[i] = in.nextInt();
+		}
 		
+		out.println(solve(N,K,slices));
 	}
-
+	public static int[] solve(int N,int K,int[] slices) {
+		//TODO:
+		return null;
+	}
 }
