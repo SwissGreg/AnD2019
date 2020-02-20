21ceeaf2d7e1cc944a17c20d1d21b0728adb0893
Greg <rgregory@student.ethz.ch>
2020-02-20 19:34:27 +0100

    GoogleHashCode2020 input/output using library and Books class

--- AnDpractice2019/src/GoogleHashCode2020.java
+++ AnDpractice2019/src/GoogleHashCode2020.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GoogleHashCode2020 {
@@ -23,15 +24,18 @@ public class GoogleHashCode2020 {
 		readSolve(new Scanner (inputE), out);
 
 	}
-	public static void readSolve(Scanner scan, PrintStream out) {
+	public static void readSolve(Scanner scan, PrintStream out) {	
+		
 		int B = scan.nextInt();
-		int L = scan.nextInt();
+		int L = scan.nextInt();	
 		int D = scan.nextInt();
+		ArrayList<Library> adj = new ArrayList<Library>();
 		//L libraries
 		for(int i = 0; i<L;i++) {
 			int n = scan.nextInt(); // number of books in library i
 			int t = scan.nextInt(); // number of days to signup library i
 			int m = scan.nextInt(); // number of books which can be shipped from i
+			Library lib = new Library(n,t,m);
 			//read in the ID's of the books
 			for(int j = 0; j<n;j++) {
 				int id = scan.nextInt();
@@ -41,5 +45,28 @@ public class GoogleHashCode2020 {
 	
 	public static void solve(int B,int L,int D) {
 		
+	}
+	
+	static class Library{
+		int n;
+		int t;
+		int m;
+		ArrayList<Books> books;
+		public Library(int n,int t,int m) {
+			this.n = n;
+			this.t = t;
+			this.m = m;
+			books = new ArrayList<Books>();
+		}
+	}
+	
+	public class Books{
+		int value;
+		int id;
+		
+		public Books(int val, int id) {
+			this.value = val;
+			this.id = id;
+		}
 	}
 }
