1f289badea4236387f89b30754f16be8c2330d43
Greg <rgregory@student.ethz.ch>
2020-02-20 19:50:07 +0100

    added libID

--- AnDpractice2019/src/GoogleHashCode2020.java
+++ AnDpractice2019/src/GoogleHashCode2020.java
@@ -13,63 +13,67 @@ public class GoogleHashCode2020 {
 		File inputD = new File("InputD.txt");
 		File inputE = new File("InputE.txt");
 		PrintStream out = new PrintStream("outputA.txt");
-		readSolve(new Scanner (inputA), out);
+		readSolve(new Scanner(inputA), out);
 		out = new PrintStream("outputB.txt");
-		readSolve(new Scanner (inputB), out);
+		readSolve(new Scanner(inputB), out);
 		out = new PrintStream("outputC.txt");
-		readSolve(new Scanner (inputC), out);
+		readSolve(new Scanner(inputC), out);
 		out = new PrintStream("outputD.txt");
-		readSolve(new Scanner (inputD), out);
+		readSolve(new Scanner(inputD), out);
 		out = new PrintStream("outputE.txt");
-		readSolve(new Scanner (inputE), out);
+		readSolve(new Scanner(inputE), out);
 
 	}
-	public static void readSolve(Scanner scan, PrintStream out) {	
-		
+
+	public static void readSolve(Scanner scan, PrintStream out) {
+
 		int B = scan.nextInt();
-		int L = scan.nextInt();	
+		int L = scan.nextInt();
 		int D = scan.nextInt();
 		ArrayList<Books> values = new ArrayList();
-		for(int i = 0; i<B;i++) {
-			Books book = new Books(scan.nextInt(),i);
+		for (int i = 0; i < B; i++) {
+			Books book = new Books(scan.nextInt(), i);
 			values.add(book);
 		}
 		ArrayList<Library> adj = new ArrayList<Library>();
-		//L libraries
-		for(int i = 0; i<L;i++) {
+		// L libraries
+		for (int i = 0; i < L; i++) {
 			int numBooks = scan.nextInt(); // number of books in library i
 			int registerTime = scan.nextInt(); // number of days to signup library i
 			int shippingAmount = scan.nextInt(); // number of books which can be shipped from i
-			Library lib = new Library(numBooks,registerTime,shippingAmount);
-			//read in the ID's of the books
-			for(int j = 0; j<numBooks;j++) {
+			Library lib = new Library(i, numBooks, registerTime, shippingAmount);
+			// read in the ID's of the books
+			for (int j = 0; j < numBooks; j++) {
 				int id = scan.nextInt();
 				lib.books.add(values.get(id));
 			}
 		}
 	}
-	
-	public static void solve(int B,int L,int D) {
-		
+
+	public static void solve(int B, int L, int D) {
+
 	}
-	
-	static class Library{
+
+	static class Library {
+		int libID;
 		int numBooks;
 		int registerTime;
 		int shippingAmount;
 		ArrayList<Books> books;
-		public Library(int n,int t,int m) {
+
+		public Library(int libID, int n, int t, int m) {
+			this.libID = libID;
 			this.numBooks = n;
 			this.registerTime = t;
 			this.shippingAmount = m;
 			books = new ArrayList<Books>();
 		}
 	}
-	
-	static class Books{
+
+	static class Books {
 		int value;
 		int id;
-		
+
 		public Books(int val, int id) {
 			this.value = val;
 			this.id = id;
